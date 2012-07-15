2e129d3de474009814d54e234690a28b3f44d775
Richard <richard@mozor.net>
2012-07-15 13:31:56 -0500

    Version 0.1, that requires hardcoding of links.

--- wp-vb-avatars/wp-vb-avatars.php
+++ wp-vb-avatars/wp-vb-avatars.php
@@ -1,8 +1,9 @@
 <?php
+
 /*
   Plugin Name: wp-vb-avatars
   Plugin URI: https://github.com/richard4339/wp-vb-avatars
-  Description: Plugin adds a new avatar source from a vBulletin table.
+  Description: Plugin replaces avatars with avatars from a vBulletin table, if it exists.
   Version: 0.1
   Author: Richard
   Author URI: http://www.digitalxero.com
@@ -23,4 +24,37 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+function site_get_avatar($avatar, $id_or_email, $size, $default, $alt) {
+    $email = '';
+    if (is_numeric($id_or_email)) {
+        $id = (int) $id_or_email;
+        $user = get_userdata($id);
+        if ($user)
+            $email = $user->user_email;
+    } elseif (is_object($id_or_email)) {
+        $email = $id_or_email->comment_author_email;
+    }
+    $forum_db = '';
+    $img_folder = ''; // No trailing slash
+    $img_path = $img_folder . '/image.php?u=';
+
+    $my_wpdb = new wpdb(DB_USER, DB_PASSWORD, $forum_db, DB_HOST);
+
+    $myrows = $my_wpdb->get_var($my_wpdb->prepare("SELECT userid
+    FROM " . $forum_db . ".vb_user
+    WHERE email = %s LIMIT 1", array($email)));
+
+    if ($myrows != '') {
+        $img = $img_path . $myrows;
+    } elseif ($avatar) {
+        return $avatar;
+    } else {
+        $img = $default;
+    }
+
+    $my_avatar = '<img src="' . $img . '" alt="' . $alt . '" height="' . $size . '" width="' . $size . '" class="avatar avatar-50 photo grav-hashed grav-hijack" />';
+    return $my_avatar;
+}
+
+add_filter('get_avatar', 'site_get_avatar', 10, 5);
 ?>
