37225c8ac2493c02989a4e591691eaf4a5c0cfdd
Richard <richard@mozor.net>
2012-07-15 10:46:50 -0500

    Initial version

---
+++ wp-vb-avatars/wp-vb-avatars.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  Plugin Name: wp-vb-avatars
+  Plugin URI: https://github.com/richard4339/wp-vb-avatars
+  Description: Plugin adds a new avatar source from a vBulletin table.
+  Version: 0.1
+  Author: Richard
+  Author URI: http://www.digitalxero.com
+ */
+
+/*  Copyright 2012  RICHARD LYNSKEY
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+?>
